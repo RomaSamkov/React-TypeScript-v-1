3a90679ad7ae3fd07f07e3b2e35de4487d7f451b
RomaSamkov <Romansamkov.rs@gmail.com>
2023-05-09 20:39:35 +0300

    Create Card.tsx

---
+++ src/components/Card.tsx
@@ -0,0 +1,34 @@
+import { useState, FC } from "react";
+
+export enum CardVariant {
+  outlined = "outlined",
+  primary = "primary",
+}
+
+interface CardProps {
+  width: string;
+  height: string;
+  children?: React.ReactNode;
+  variant: CardVariant;
+  onClick: (num: number) => void;
+}
+
+const Card: FC<CardProps> = ({ width, height, variant, children, onClick }) => {
+  const [state, setState] = useState(0);
+  return (
+    <div
+      style={{
+        width,
+        height,
+        border: variant === CardVariant.outlined ? "1px solid green" : "none",
+        background: variant === CardVariant.primary ? "lightgreen" : "",
+        borderRadius: "5px",
+      }}
+      onClick={() => onClick(state)}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Card;
