808ec12cfcf58b8a5de175f35332df4945b6fcf4
RomaSamkov <Romansamkov.rs@gmail.com>
2023-05-09 20:39:41 +0300

    add Card

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Card, { CardVariant } from "./components/Card";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Card
+        onClick={(num) => console.log("Click", num)}
+        width="100px"
+        height="100px"
+        variant={CardVariant.primary}
+      >
+        <button>Click</button>
+      </Card>
+    </div>
+  );
 }
 
-export default App
+export default App;
