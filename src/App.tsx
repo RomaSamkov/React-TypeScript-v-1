e3eb5c3f2e4655486af9ce7e744e14ad501b3729
RomaSamkov <Romansamkov.rs@gmail.com>
2023-05-10 20:00:56 +0300

    add fetchUsers

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,27 @@
+import { useState, useEffect } from "react";
 import Card, { CardVariant } from "./components/Card";
 import UserList from "./components/UserList";
+import { IUser } from "./types/types";
+import axios from "axios";
 
 function App() {
-  const users = [
-    {
-      id: 1,
-      name: "Leanne Graham",
-      username: "Bret",
-      email: "Sincere@april.biz",
-      address: {
-        street: "Kulas Light",
-        suite: "Apt. 556",
-        city: "Gwenborough",
-        zipcode: "92998-3874",
-      },
-    },
-    {
-      id: 2,
-      name: "Ervin Howell",
-      username: "Antonette",
-      email: "Shanna@melissa.tv",
-      address: {
-        street: "Victor Plains",
-        suite: "Suite 879",
-        city: "Wisokyburgh",
-        zipcode: "90566-7771",
-      },
-    },
-  ];
+  const [users, setUsers] = useState<IUser[]>([]);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  async function fetchUsers() {
+    try {
+      const response = await axios.get<IUser[]>(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      setUsers(response.data);
+    } catch (error) {
+      alert(error);
+    }
+  }
+
   return (
     <div>
       <Card
