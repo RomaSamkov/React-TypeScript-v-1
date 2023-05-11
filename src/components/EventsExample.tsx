73e6a28c0b5c0249a9d1785e74cbd9ffd67e607c
RomaSamkov <Romansamkov.rs@gmail.com>
2023-05-11 18:31:25 +0300

    add useRef

--- src/components/EventsExample.tsx
+++ src/components/EventsExample.tsx
@@ -1,20 +1,23 @@
-import React, { FC, useState } from "react";
+import React, { FC, useRef, useState } from "react";
 
 const EventsExample: FC = () => {
   const [value, setValue] = useState<string>("");
   const [isDrag, setIsDrag] = useState<boolean>(false);
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
   };
 
   const clickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e;
-    console.log(value);
+    console.log(e);
+
+    console.log("ref:", inputRef.current?.value);
   };
 
   const dragHandler = (e: React.DragEvent<HTMLDivElement>) => {
     console.log("Drag");
+    console.log(e);
   };
 
   const dropHandler = (e: React.DragEvent<HTMLDivElement>) => {
@@ -35,7 +38,13 @@ const EventsExample: FC = () => {
 
   return (
     <div>
-      <input value={value} onChange={changeHandler} type="text" />
+      <input
+        value={value}
+        onChange={changeHandler}
+        type="text"
+        placeholder="JS"
+      />
+      <input ref={inputRef} type="text" placeholder="TS" />
       <button onClick={clickHandler}>Click</button>
       <div
         onDrag={dragHandler}
