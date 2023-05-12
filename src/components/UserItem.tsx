48cd04bb9fa3b222c5b849bcab541f10f652e7ff
RomaSamkov <Romansamkov.rs@gmail.com>
2023-05-12 18:33:00 +0300

    add onClick

--- src/components/UserItem.tsx
+++ src/components/UserItem.tsx
@@ -3,11 +3,12 @@ import { IUser } from "../types/types";
 
 interface UserItemProps {
   user: IUser;
+  onClick: (user: IUser) => void;
 }
 
-const UserItem: FC<UserItemProps> = ({ user }) => {
+const UserItem: FC<UserItemProps> = ({ user, onClick }) => {
   return (
-    <div key={user.id}>
+    <div onClick={() => onClick(user)} key={user.id}>
       {user.id}. with name {user.name} lived in the city {user.address.city} on
       the street {user.address.street}
     </div>
